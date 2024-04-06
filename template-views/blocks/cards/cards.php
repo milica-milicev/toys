45eec16b74d659809ca2f98ee5175ada104ffb62
Milica Milicev <milica.milicev13@gmail.com>
2024-04-06 18:26:53 +0200

    updates acf

--- template-views/blocks/cards/cards.php
+++ template-views/blocks/cards/cards.php
@@ -19,45 +19,40 @@
 						// Dobijanje URL-a kategorije
 						$term_link = get_term_link($term);
 						$term_name = $term->name;
-						$thumbnail_data = get_field('background_thumbnail', 'product_cat_' . $category_id);
 						$short_desc = get_field('short_description', 'product_cat_' . $category_id);
 						$card_theme = get_field('card_theme', 'product_cat_' . $category_id);
-						// $background_img
+						$background_img = get_field('background_img', 'product_cat_' . $category_id);
+						$img_in_corner = get_field('img_in_corner', 'product_cat_' . $category_id);
+						
+						if ($background_img === 'full_img') :
+							$thumbnail_data = get_field('background_thumb', 'product_cat_' . $category_id);
+							// Zatim, proverite da li vrednost postoji i da li je to niz sa ključem 'url'
+							if (is_array($thumbnail_data) && isset($thumbnail_data['url'])) :
+								$background = 'background-image: url(' . $thumbnail_data['url'] . ')';
+							else :
+								// Ako nema slike, koristite placeholder
+								$background = 'background-image: ' . get_stylesheet_directory_uri() . '/assets/images/placeholder.jpg';
+							endif;
+						else : 
+							$background_color = $img_in_corner['background_color'];
+							$image = $img_in_corner['image']['url'];
+							$background = 'background-color: ' . $background_color;
+						endif; ?>
 
-						// Zatim, proverite da li vrednost postoji i da li je to niz sa ključem 'url'
-						if (is_array($thumbnail_data) && isset($thumbnail_data['url'])) :
-							$image_url = $thumbnail_data['url'];
-						else :
-							// Ako nema slike, koristite placeholder
-							$image_url = get_stylesheet_directory_uri() . '/assets/images/placeholder.jpg';
-						endif;
-
-						if ( $i % 2 !== 0) : ?>
-							<div class="card-item">
-								<a href="<?php echo $term_link; ?>">
-									<div class="card-item__wrap" style="background-color: #17C5CE;">
-										<img class="card-item__img" src="<?php echo $image_url; ?>" alt="">
-										<p class="card-item__pretitle card-item__pretitle--<?php echo $card_theme; ?>"><?php echo $short_desc; ?></p>
-										<h3 class="card-item__title"><?php echo $term_name; ?></h3>
-										<div class="card-item__btn">
-											<span class="btn btn--white">Pogledaj ponudu</span>
-										</div>
+						<div class="card-item">
+							<a href="<?php echo $term_link; ?>">
+								<div class="card-item__wrap" style="<?php echo $background ; ?>">
+									<?php if ($background_img === 'corner_img') : ?>
+										<img class="card-item__img" src="<?php echo $image; ?>" alt="">
+									<?php endif; ?>
+									<p class="card-item__pretitle card-item__pretitle--<?php echo $card_theme; ?>"><?php echo $short_desc; ?></p>
+									<h3 class="card-item__title card-item__title--<?php echo $card_theme; ?>"><?php echo $term_name; ?></h3>
+									<div class="card-item__btn">
+										<span class="btn btn--white">Pogledaj ponudu</span>
 									</div>
-								</a>
-							</div>
-						<?php else : ?>
-							<div class="card-item">
-								<a href="<?php echo $term_link; ?>">
-									<div class="card-item__wrap" style="background-image: url('<?php echo $image_url; ?>');">
-										<p class="card-item__pretitle card-item__pretitle--<?php echo $card_theme; ?>"><?php echo $short_desc; ?></p>
-										<h3 class="card-item__title card-item__title--dark"><?php echo $term_name; ?></h3>
-										<div class="card-item__btn">
-											<span class="btn btn--white">Pogledaj ponudu</span>
-										</div>
-									</div>
-								</a>
-							</div>
-						<?php endif; ?>
+								</div>
+							</a>
+						</div>
 					<?php endif; ?>
 				<?php endfor; ?>
 			<?php endif; ?>
